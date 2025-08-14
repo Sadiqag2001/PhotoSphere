ee33b53a60638376446c081c1e4ef79d194ab0ac
Abubakar Ado Garba <abubakaradogarba@gmail.com>
2025-08-14 21:17:12 +0100

    Pages and components created, NavBar completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { ImSearch } from "react-icons/im";
+
+function NavBar() {
+  return (
+    <div className='flex flex-row justify-between p-4'>
+        <div className='flex flex-row justify-between items-center gap-8'>
+            <h1 className='text-4xl text-gray-900'>PhotoSphere</h1>
+        </div>
+        <div className='relative w-full sm:w-auto '> 
+            <input type="text" placeholder='What are you looking for? ' className='lg:pl-5 lg:pr-40 sm:pl-3 sm:pr-10 md:pl-5 md:pr-12 py-3 rounded-3xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300' />
+            <button className='absolute right-2 top-1/2 -translate-y-1/2 bg-blue-500 text-white p-2 rounded-full hover:bg-blue-600'> <ImSearch/></button>
+        </div>
+        <div className='flex flex-row gap-6 justify-center items-center mr-6 cursor-pointer'>
+            <p className=' hover:text-gray-300 transform transition duration-200'>Home</p>
+            <p className=' hover:text-gray-300 transform transition duration-200'>Explore</p>
+            <p className=' hover:text-gray-300 transform transition duration-200'>Favourites</p>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
